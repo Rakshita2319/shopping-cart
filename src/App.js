4a19645e67d3ae62bdba6a46b88b187f784bafa6
Rakshita <bhattrakshita13@gmail.com>
2023-05-09 01:09:38 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import 
+{BrowserRouter,
+Routes,
+Route
+} from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+//IMPORT ALL COMPONENTS
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+
+
+//MAIN CSS
+import "./styles/app.scss"; 
+
+//ALL CSS
+import "./styles/cart.scss"
+import "./styles/colors.scss"
+import "./styles/header.scss"
+import "./styles/home.scss"
+import "./styles/mediaquery.scss"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+   <BrowserRouter>
+        <Header/>
+         <Routes>
+            <Route exact path='/' element={<Home/>}/>
+            <Route exact path='/cart' element={<Cart/>}/>
+          </Routes>
+          <Toaster/>
+    </BrowserRouter>
+    
+
+
   );
 }
 
